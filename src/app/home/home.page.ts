df49ec2cbc018f4f7f52da9d0334c62867cf4aee
sandtell <sandtellcustomercare@gmail.com>
2020-01-28 13:37:59 +0530

    custom svg

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,4 +10,12 @@ export class HomePage {
 
   constructor() {}
   
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
 }
